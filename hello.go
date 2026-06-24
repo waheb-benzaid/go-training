6e0ce4e540275efba25c3a4ae9b4ca7be43af5ea
waheb-benzaid <waheb.benzaid@gmail.com>
2026-06-24 20:22:23 +0100

    feat: add name parameter to Hello function with default world fallback

--- hello.go
+++ hello.go
@@ -2,10 +2,15 @@ package main
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func main() {
-	fmt.Println(Hello())
+	fmt.Println(Hello("waheb"))
 }
 
-func Hello() string {
-	return "Hello World"
+func Hello(name string) string {
+	if name == "" {
+		return "Hello, world"
+	}
+	return englishHelloPrefix + name
 }
--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello World"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("waheb")
+		want := "Hello, waheb"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != want {
+			t.Errorf("got %q but want %q", got, want)
+		}
+	})
+
+	t.Run("Say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+
+		if got != want {
+			t.Errorf("got %q but want %q", got, want)
+		}
+	})
 }
