0c1f99ec3e16ea4f4e18fc85b8ffd6c0e2f8a8cb
waheb-benzaid <waheb.benzaid@gmail.com>
2026-07-01 16:57:24 +0100

    refacoring: improve test by removing the code duplication

--- hello_test.go
+++ hello_test.go
@@ -6,18 +6,20 @@ func TestHello(t *testing.T) {
 	t.Run("saying hello to people", func(t *testing.T) {
 		got := Hello("waheb")
 		want := "Hello, waheb"
-
-		if got != want {
-			t.Errorf("got %q but want %q", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("Say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
 		got := Hello("")
 		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
 
-		if got != want {
-			t.Errorf("got %q but want %q", got, want)
-		}
 	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q but want %q", got, want)
+	}
 }
